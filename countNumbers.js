4f1cb23c7f316f024f31fe49c23b2c0aea723109
Yunus ŞAHİN <81877863+yunusssahin@users.noreply.github.com>
2021-11-08 12:06:19 +0300

    Update countNumbers.js

--- countNumbers.js
+++ countNumbers.js
@@ -1,12 +1,12 @@
 let sumOfDiveners1 = 1; //her sayının kendi ile bölümü 1'dir. 1 vererek for döngüsünü numbers/2 yapıyorum
 let sumOfDiveners2 = 1;
 let isCountNumbers = (numbers1,numbers2) =>{
-    for (let i = 2 ; i < numbers1/2 +1 ; i++) {
+    for (let i = 2 ; i <= numbers1/2 ; i++) {
        if(numbers1 % i == 0){
             sumOfDiveners1 = sumOfDiveners1 + (numbers1/i)
         }
     }
-    for (let i = 2 ; i < numbers2/2 +1 ; i++) {
+    for (let i = 2 ; i <= numbers2/2 ; i++) {
         if(numbers2 % i == 0){
             sumOfDiveners2 = sumOfDiveners2 + (numbers2/i)
         }
