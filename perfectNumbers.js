f4516dd7a88792f923272a73620509420d0d2b27
Yunus ŞAHİN <81877863+yunusssahin@users.noreply.github.com>
2021-11-08 11:40:20 +0300

    Add files via upload
    Finding perect numbers

---
+++ perfectNumbers.js
@@ -0,0 +1,25 @@
+
+let sumOfDiveners = 1
+let perfectNumbers = []
+let flag = 0
+let findingPerfectNum = (number)=>{
+for (let i = number; i > 1; i--) {
+   for(let j = 2 ; j < i/2 + 1; j++){
+       if(i % j == 0){
+           sumOfDiveners = sumOfDiveners + j
+   }
+}
+   if(sumOfDiveners == i){
+       perfectNumbers[flag] = i
+       sumOfDiveners = 1
+       flag++
+   }else{
+    sumOfDiveners = 1
+}
+}
+}
+
+findingPerfectNum(1000)
+for (let i = 0; i < perfectNumbers.length; i++) {
+    console.log(perfectNumbers[i] + ": Mükemmel sayıdır.")    
+}
